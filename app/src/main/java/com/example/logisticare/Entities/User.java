693f1323773e04ff45c0c98fef4fe882caaf43e2
avihu damari <tchvutsntrh@gmail.com>
2019-12-03 15:57:33 +0200

    User class version1

--- app/src/main/java/com/example/logisticare/Entities/User.java
+++ app/src/main/java/com/example/logisticare/Entities/User.java
@@ -1,4 +1,62 @@
 package com.example.logisticare.Entities;
 
+import android.location.Location;
+
 public class User {
+
+    String phone_number;
+    String first_name;
+    String last_name;
+    String mail_address;
+    Location address;
+
+    public User(String phone_number, String first_name, String last_name, String mail_address, Location address) {
+        this.phone_number = phone_number;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.mail_address = mail_address;
+        this.address = address;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getMail_address() {
+        return mail_address;
+    }
+
+    public void setMail_address(String mail_address) {
+        this.mail_address = mail_address;
+    }
+
+    public Location getAddress() {
+        return address;
+    }
+
+    public void setAddress(Location address) {
+        this.address = address;
+    }
+
+
 }
